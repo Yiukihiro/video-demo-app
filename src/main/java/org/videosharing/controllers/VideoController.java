8aff12eba002729a761ebc6ce6da2bd104ebb9b4
Vladislav Butenko <butenko.vlad97@gmail.com>
2024-12-07 04:03:03 +0300

    add authentication handmade DB data

--- src/main/java/org/videosharing/controllers/VideoController.java
+++ src/main/java/org/videosharing/controllers/VideoController.java
@@ -8,7 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.videosharing.domain.VideoModel;
 import org.videosharing.dto.VideoInfoDto;
 import org.videosharing.services.VideoService;
 
